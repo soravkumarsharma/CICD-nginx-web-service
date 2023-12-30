9d9485876bcea04905e698fb85aa438b96403891
Sorav Kumar Sharma <add.sorav@gmail.com>
2023-12-30 13:23:57 +0530

    Update start_cont.sh

--- scripts/start_cont.sh
+++ scripts/start_cont.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Pull the docker image from the dockerHub.
-docker push soravkumarsharma/nginx-staticwebpage:latest
+docker pull soravkumarsharma/nginx-staticwebpage:latest
 
 #Run the docker pulled docker image.
 docker run -d -p 80:80 soravkumarsharma/nginx-staticwebpage:latest

aea03f4dedca6f86a3f653c12fe0228b576c3a12
Sorav Kumar Sharma <add.sorav@gmail.com>
2023-12-30 14:21:26 +0530

    update stop_cont.sh

--- scripts/stop_cont.sh
+++ scripts/stop_cont.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 #docker container stopped 
-docker stop nginx-web
-docker rm nginx-web
-docker rmi soravkumarsharma/nginx-staticwebpage:latest 
+containerid = `docker ps | awsk -F " " '{print $1}'`
+docker stop containerid
+docker rm containerid 
+docker rmi $(docker images -a -q)

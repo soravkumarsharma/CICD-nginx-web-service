33871e111e440e4beee27745c0d3985fbcdeea21
Sorav Kumar Sharma <add.sorav@gmail.com>
2024-01-20 10:06:47 +0530

    deleted unnecessary files

--- scripts/start_cont.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  
- 
-#Pull the docker image from the dockerHub.
-docker pull soravkumarsharma/nginx-staticwebpage:latest
-
-#Run the docker pulled docker image.
-docker run -d -p 80:80 soravkumarsharma/nginx-staticwebpage:latest
--- scripts/stop_cont.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-#docker container stopped 
-containerid = `docker ps | awk -F " " '{print $1}'`
-docker stop containerid
-docker rm containerid 
-docker rmi $(docker images -a -q)
